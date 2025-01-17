7c3beffb63416527d6423ef549119911e1742a72
V-Gulev <velizargulev@abv.bg>
2025-01-17 16:34:18 +0200

    CitiesByContinentAndCountry_07

--- SoftUni/CitiesByContinentAndCountry_07.java
+++ SoftUni/CitiesByContinentAndCountry_07.java
@@ -6,24 +6,31 @@ public class CitiesByContinentAndCountry_07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        LinkedHashMap<String, LinkedHashMap<String, LinkedHashSet<String>>> world = new LinkedHashMap<>();
+        LinkedHashMap<String, LinkedHashMap<String, ArrayList<String>>> world = new LinkedHashMap<>();
 
-        for (int i = 0; i < n; i++) {
+        while (n-- > 0){
             String[] input = sc.nextLine().trim().split("\\s+");
             String continent = input[0];
             String country = input[1];
             String city = input[2];
 
-            world.putIfAbsent(continent, new LinkedHashMap<>());
-            world.get(continent).putIfAbsent(country, new LinkedHashSet<>());
-            world.get(continent).get(country).add(city);
+            if (!world.containsKey(continent)) {
+                world.put(continent, new LinkedHashMap<>() {{put(country, new ArrayList<>(){{add(city);}});}});
+            } else {
+                if (!world.get(continent).containsKey(country)) {
+                    world.get(continent).put(country, new ArrayList<>(){{add(city);}});
+                } else {
+                    world.get(continent).get(country).add(city);
+                }
+            }
         }
 
-        for (Map.Entry<String, LinkedHashMap<String, LinkedHashSet<String>>> entry : world.entrySet()) {
+        for (Map.Entry<String, LinkedHashMap<String, ArrayList<String>>> entry : world.entrySet()) {
             System.out.println(entry.getKey() + ":");
-            for (Map.Entry<String, LinkedHashSet<String>> country : entry.getValue().entrySet()) {
+            for (Map.Entry<String, ArrayList<String>> country : entry.getValue().entrySet()) {
                 System.out.println("  " + country.getKey() + " -> " + String.join(", ", country.getValue()));
             }
         }
     }
 }
+
